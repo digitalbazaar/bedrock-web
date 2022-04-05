5c4ae01b6b690fb44a93b0d7810d487f6b642376
Dave Longley <dlongley@digitalbazaar.com>
2022-04-05 13:02:00 -0400

    Improve test coverage.

--- test/web/10-api.js
+++ test/web/10-api.js
@@ -5,6 +5,15 @@ import {config, extend, isObject, ready, start, waitUntil} from '@bedrock/web';
 
 describe('bedrock-web API', () => {
   describe('API', () => {
+    it('should wait for a promise to resolve', async () => {
+      let waited = false;
+      waitUntil(new Promise(resolve => setTimeout(() => {
+        waited = true;
+        resolve();
+      }), 100));
+      await start();
+      waited.should.equal(true);
+    });
     it('exports the proper APIs', async () => {
       should.exist(config);
       config.should.be.an('object');
@@ -38,6 +47,58 @@ describe('bedrock-web API', () => {
       };
       result.should.deep.equal(expect);
       target.should.deep.equal(expect);
+    });
+    it('should deep extend an object with array source', async () => {
+      const target = {a: 1, foo: {bar: 1}, baz: {x: 1}};
+      const source = [{foo: {bar: 2}, baz: {y: 2}}];
+      const result = extend({target, source, deep: true});
+      const expect = {
+        a: 1, foo: {bar: 2}, baz: {x: 1, y: 2}
+      };
+      result.should.deep.equal(expect);
+      target.should.deep.equal(expect);
+    });
+    it('should deep extend an object with no existing key', async () => {
+      const target = {a: 1, foo: {bar: 1}};
+      const source = {foo: {bar: 2}, baz: {y: 2}};
+      const result = extend({target, source, deep: true});
+      const expect = {
+        a: 1, foo: {bar: 2}, baz: {y: 2}
+      };
+      result.should.deep.equal(expect);
+      target.should.deep.equal(expect);
+    });
+    it('should fail to extend a non-object', async () => {
+      const target = {};
+      const source = 'non-object';
+      let err;
+      try {
+        extend({target, source});
+      } catch(e) {
+        err = e;
+      }
+      should.exist(err);
+      err.name.should.equal('TypeError');
+      err.message.should.equal(
+        '"source" must be an object or an array of objects.');
+    });
+    it('should fail on empty params', async () => {
+      let err;
+      try {
+        extend();
+      } catch(e) {
+        err = e;
+      }
+      should.exist(err);
+      err.name.should.equal('TypeError');
+    });
+    it('should detect an object', async () => {
+      const result = isObject({});
+      result.should.equal(true);
+    });
+    it('should detect a non-object', async () => {
+      const result = isObject('');
+      result.should.equal(false);
     });
   }); // end API
 });
