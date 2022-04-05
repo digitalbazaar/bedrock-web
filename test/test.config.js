885870c93c90d05e595c5198befda450723ad6c8
Dave Longley <dlongley@digitalbazaar.com>
2022-04-05 11:27:22 -0400

    Convert to module (ESM).

--- test/test.config.js
+++ test/test.config.js
@@ -1,10 +1,8 @@
 /*!
- * Copyright (c) 2019-2020 Digital Bazaar, Inc. All rights reserved.
+ * Copyright (c) 2019-2022 Digital Bazaar, Inc. All rights reserved.
  */
-'use strict';
-
-const {config} = require('bedrock');
-const path = require('path');
+import {config} from '@bedrock/core';
+import path from 'path';
 
 config.karma.suites['bedrock-web'] = path.join('web', '**', '*.js');
 
@@ -12,4 +10,3 @@ config.karma.config.proxies = {
   '/': 'https://localhost:18443'
 };
 config.karma.config.proxyValidateSSL = false;
-
